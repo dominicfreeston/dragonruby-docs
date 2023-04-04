60ecc5e95175ee163bce0960beb3e90576d63199
Dominic <dominic.freeston@gmail.com>
2023-04-04 22:49:39 +0100

    Try to make bb and bootleg available in PATH but keep it local

--- build.sh
+++ build.sh
@@ -3,16 +3,16 @@
 echo "Creating and moving to temp directory"
 mkdir temp
 cd temp
+export PATH=$PATH:$(pwd)
 
 echo "Installing Babashka"
 curl -sLO https://raw.githubusercontent.com/babashka/babashka/master/install
 chmod +x install
-./install --dir ~/bin
+./install --dir .
 
 echo "Installing bootleg"
 curl -LO https://github.com/retrogradeorbit/bootleg/releases/download/v0.1.9/bootleg-0.1.9-linux-amd64.tgz
 tar xvf bootleg-0.1.9-linux-amd64.tgz
-mv bootleg ~/bin
 
 echo "Returning to root and running build task"
 cd ..
